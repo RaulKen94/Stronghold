b12a044f77642d24176a8f93d46e7bb42ec2a72a
RaulKen94 <raulken94@gmail.com>
2026-08-18 23:22:06 +0200

    Remove Supabase connection test function
    Removed the testSupabase function and its invocation.

--- main.js
+++ main.js
@@ -2,24 +2,6 @@
     window.Roccaforte = window.Roccaforte || {};
     var NS = window.Roccaforte;
 
-    /*
-    async function testSupabase() {
-        if (!NS.supabase) {
-            alert('Supabase non inizializzato. Controlla supabase-config.js');
-            return;
-        }
-        const { data, error } = await NS.supabase
-            .from('rooms')
-            .select('*');
-        if (error) {
-            alert('Errore connessione Supabase: ' + JSON.stringify(error));
-        } else {
-            alert('Connessione Supabase OK! Stanze presenti: ' + JSON.stringify(data));
-        }
-    }
-
-    // Esegui il test e poi mostra il menu
-    testSupabase().finally(() => {*/
         window.showMainMenu();
-    //});
+    
 })();
