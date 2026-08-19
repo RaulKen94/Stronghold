aa73c5da03288bf60f7fd3d0e76f54826911aa88
RaulKen94 <raulken94@gmail.com>
2026-08-19 08:52:34 +0200

    Create game-multiplayer-adapter.js for multiplayer mode
    Initial placeholder implementation for multiplayer support.

---
+++ game-multiplayer-adapter.js
@@ -0,0 +1,11 @@
+/**
+ * GAME-MULTIPLAYER-ADAPTER.JS
+ * Adatterà la classe Game per supportare la modalità multiplayer.
+ * Al momento è un segnaposto per la Fase 4.
+ */
+(function() {
+    window.Roccaforte = window.Roccaforte || {};
+    var NS = window.Roccaforte;
+    // Nessuna logica ancora implementata
+    console.log('Game Multiplayer Adapter caricato (Fase 4 non attiva)');
+})();
