9b4e132dd5d21a180e472d3b80cb6269e8b64d51
RaulKen94 <raulken94@gmail.com>
2026-08-20 19:36:38 +0200

    Remove JavaScript error handling function
    Removed global error handler for JavaScript errors.

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-window.onerror = function(message, source, lineno, colno, error) {
-    alert('Errore JavaScript:\n' + message + '\nFile: ' + source + '\nRiga: ' + lineno);
-    return true;
-};
-
 (function() {
     window.Roccaforte = window.Roccaforte || {};
     var NS = window.Roccaforte;
