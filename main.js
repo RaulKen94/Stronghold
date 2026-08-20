67468b1f793d2f2d1623e8648065e4bc9fb1b40e
RaulKen94 <raulken94@gmail.com>
2026-08-20 17:37:13 +0200

    Implement global error handler for JavaScript
    Added global error handler to display JavaScript errors.

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
+window.onerror = function(message, source, lineno, colno, error) {
+    alert('Errore JavaScript:\n' + message + '\nFile: ' + source + '\nRiga: ' + lineno);
+    return true;
+};
+
 (function() {
     window.Roccaforte = window.Roccaforte || {};
     var NS = window.Roccaforte;
 
-        window.showMainMenu();
-    
+    window.showMainMenu();
 })();
